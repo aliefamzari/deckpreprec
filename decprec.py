0df5891b1c0dbcf07742b975c72fbae564a4feb1
aliefamzari <almaz@PC105106.de-prod.dk>
2025-12-15 22:36:13 +0800

    a

--- decprec.py
+++ decprec.py
@@ -1270,14 +1270,18 @@ def main_menu(folder):
             
             # Clear track list area if scroll position changed
             if scroll_offset != last_scroll_offset:
-                # Clear the track list region (14 lines to cover all tracks + indicators)
+                # Clear the track list region completely
                 for clear_y in range(track_start_y, track_start_y + 14):
-                    safe_addstr(stdscr, clear_y, 0, " " * min(70, max_x - 2), 0)
+                    try:
+                        stdscr.move(clear_y, 0)
+                        stdscr.clrtoeol()
+                    except:
+                        pass
                 last_scroll_offset = scroll_offset
             
             # Show scroll indicators
             if scroll_offset > 0:
-                safe_addstr(stdscr, track_start_y, 0, "  ↑ More tracks above... " + " " * 45, curses.color_pair(COLOR_CYAN) | curses.A_DIM)
+                safe_addstr(stdscr, track_start_y, 0, "  ↑ More tracks above...", curses.color_pair(COLOR_CYAN) | curses.A_DIM)
                 track_display_start = track_start_y + 1
             else:
                 track_display_start = track_start_y
@@ -1290,7 +1294,7 @@ def main_menu(folder):
                 
                 selected_marker = "●" if track in selected_tracks else "○"
                 highlight_marker = "▶" if i == current_index else " "
-                preview_marker = " ♪" if i == previewing_index else ""  # Musical note for playing track
+                preview_marker = " ♪" if i == previewing_index else ""
                 duration_str = format_duration(track['duration'])
                 is_current = i == current_index
                 is_selected = track in selected_tracks
@@ -1310,23 +1314,19 @@ def main_menu(folder):
                     text_color = COLOR_WHITE
                     attr = 0
                 
-                # Truncate long filenames to prevent wrapping
-                max_name_len = max_x - 25  # Leave space for markers, number, duration
+                # Truncate filename to fit screen (leave 20 chars for markers + duration)
                 track_name = track['name']
-                if len(track_name) > max_name_len:
+                max_name_len = max_x - 20
+                if len(track_name) > max_name_len and max_name_len > 10:
                     track_name = track_name[:max_name_len - 3] + "..."
                 
                 track_line = f"{highlight_marker} {selected_marker} {i + 1:02d}. {track_name} - {duration_str}{preview_marker}"
-                # Pad with spaces to clear any previous content
-                padded_line = track_line + " " * (min(70, max_x - 2) - len(track_line))
-                safe_addstr(stdscr, track_y, 0, padded_line, curses.color_pair(text_color) | attr)
+                safe_addstr(stdscr, track_y, 0, track_line, curses.color_pair(text_color) | attr)
             
             # Show bottom scroll indicator
             if visible_end < len(tracks):
-                indicator = f"  ↓ {len(tracks) - visible_end} more tracks below... "
-                padded_indicator = indicator + " " * (min(70, max_x - 2) - len(indicator))
                 safe_addstr(stdscr, track_display_start + (visible_end - scroll_offset), 0, 
-                           padded_indicator, 
+                           f"  ↓ {len(tracks) - visible_end} more tracks below...", 
                            curses.color_pair(COLOR_CYAN) | curses.A_DIM)
                 sel_y = track_display_start + (visible_end - scroll_offset) + 2
             else:
